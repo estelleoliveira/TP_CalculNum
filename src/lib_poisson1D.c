6e181014f2e96790624b6a3042bd6d9b1927c84b
Estelle <estelle.oliveira@ens.uvsq.fr>
2024-12-17 17:38:42 +0100

    Optimised GB functions
    Global initialisation removed
    kv > 0, lead to additional line of zeros

--- src/lib_poisson1D.c
+++ src/lib_poisson1D.c
@@ -6,37 +6,36 @@
 #include "lib_poisson1D.h"
 
 void set_GB_operator_colMajor_poisson1D(double* AB, int *lab, int *la, int *kv){
-  //printf("la = %d\n", *la);
-  //printf("lab = %d\n", *lab);
-  
-  for (int i = 0; i < *la * *lab; i++) {
-    AB[i] = 0.0;
-    //printf("kv = %d\n", *kv);
-  }
-  
 
   for (int i = 0; i < *la; i++) {
-    //printf("kv : %d\n", *kv);
-    AB[*kv + i * (*lab)] = -1.0;
+    if (*kv > 0) {
+      AB[i * (*lab)] = 0.0; //si kv = 0, on ne rajoute pas de ligne de 0.0 à notre matrice bande
+    }
+    AB[*kv + i * (*lab)] = -1.0;    //correspond à A[0] pour kv = 0 et A[1] pour kv = 1 (i=0)
     AB[*kv + i * (*lab) + 1] = 2.0;
     AB[*kv + i * (*lab) + 2] = -1.0; 
+    
   }
 
-  AB[*kv] = 0.0;
-  AB[((*lab) * (*la)) - 1] = 0.0;
-    
+  AB[*kv] = 0.0;  //remplace la première valeur par 0.0 (ici on met kv pour remplacer la valeur 0 ou 1)
+  AB[((*lab) * (*la)) - 1] = 0.0; //remplace la dernière valeur par 0.0
+  
+  /*for (int j = 0; j < *la * *lab; j++) {
+    printf("AB : %f\n", AB[j]);
+    printf("kv : %d\n", *kv);
+  }*/
 }
 
 void set_GB_operator_colMajor_poisson1D_Id(double* AB, int *lab, int *la, int *kv){
-  //printf("la = %d\n", *la);
-  //printf("lab = %d\n", *lab);
-  for (int i = 0; i <  *la * *lab; i++) {
-    AB[i] = 0.0;
-  }
   
-
+//on utilise le même fonctionnement que pour la fonction précedente en modifiant les valeurs remplies
   for (int i = 0; i < *la; i++) {
+    if (*kv > 0) {
+      AB[i * (*lab)] = 0.0;
+    }
+    AB[*kv + i * (*lab)] = 0.0;
     AB[*kv + i * (*lab) + 1] = 1.0;
+    AB[*kv + i * (*lab) + 2] = 0.0;
   }
 }
 
