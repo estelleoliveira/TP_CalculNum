a3b319f54ce2417a02ac962674a589c4f4c58285
Estelle <estelle.oliveira@ens.uvsq.fr>
2024-12-27 21:55:07 +0100

    Implemented eigmax and eigmin functions
    Implemented richardson_alpha_opt using this functions

--- part2/src/lib_poisson1D_richardson.c
+++ part2/src/lib_poisson1D_richardson.c
@@ -6,18 +6,26 @@
 #include "lib_poisson1D.h"
 
 void eig_poisson1D(double* eigval, int *la){
+  double h = 1.0 / (*la + 1);  //pas entre les points de la grille
+  eigval[*la];
+  for (int k = 0; k < *la; k++){
+    eigval[k + 1] = 4 * (sin(((double) k + 1.0) * M_PI * h / 2))*(sin(((double) k + 1.0) * M_PI * h / 2));
+    printf("eigval %f\n", eigval[k]);
+  }
 }
 
 double eigmax_poisson1D(int *la){
-  return 0;
+  double h = 1.0 / (*la + 1);
+  return 4 * (sin(*la * M_PI * h / 2))*(sin(*la * M_PI * h / 2));
 }
 
 double eigmin_poisson1D(int *la){
-  return 0;
+  double h = 1.0 / (*la + 1);
+  return 4 * (sin(M_PI * h / 2))*(sin(M_PI * h / 2));
 }
 
 double richardson_alpha_opt(int *la){
-  return 0;
+  return 2.0/(eigmin_poisson1D(la) + eigmax_poisson1D(la));
 }
 
 void richardson_alpha(double *AB, double *RHS, double *X, double *alpha_rich, int *lab, int *la,int *ku, int*kl, double *tol, int *maxit, double *resvec, int *nbite){
